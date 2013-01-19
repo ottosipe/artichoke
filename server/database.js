9df578663fbd17c2ba90e5c21a4c9b084c3891bc
Otto Sipe <ottosipe@gmail.com>
2013-01-18 20:57:48 -0500

    re-init commit

---
+++ server/database.js
@@ -0,0 +1,19 @@
+var MongoDB 	= require("mongodb"),
+	secret 		= require("./secret.js");
+
+var API = module.exports = exports;
+
+(function(mongo) {
+	var Server		= mongo.Server(secret.db.url, secret.db.port)
+		, Database	= mongo.Db(secret.db.name, Server, {safe: false});
+
+	API.connect = function initMongoDB(cb) {
+		console.log('Starting DB Connection');
+		Database.open(function(err, db) {
+			if(err) { return cb(err); }
+			Database.authenticate(secret.db.user, secret.db.pass, cb);
+			API.db = Database;
+		});
+	};
+
+})(MongoDB);
---
+++ server/email.js
@@ -0,0 +1,39 @@
+var nodemailer = require("nodemailer"),
+    secret     = require(__dirname + "/secret.js"),
+    fs         = require("fs"),
+    jade       = require("jade");
+
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP", {
+    service: "Mandrill",
+    auth: secret.email // {user,pass}
+});
+
+exports.send = function sendMail(user, temp, cb) {
+    // send mail with defined transport object
+    if(user == null) return;
+    var template = jade.compile(fs.readFileSync(__dirname+'/email/' + temp, 'utf8'));
+    var html = template({
+        name: user.name,
+        email: user.email
+    });
+
+    var opts = {
+        from: "This Website <email@domain.com>", // sender address
+        to: user.name+" <"+user.email+">", // list of receivers
+        subject: "Testing...", // Subject line
+        html: html, // html body
+        generateTextFromHTML: true
+    }
+    console.log(opts);
+
+    // send the email
+    smtpTransport.sendMail(opts, function(error, response){
+        if(error){
+            cb(error);
+        } else {
+            var msg = "Message sent: " + response.message;
+            cb(msg);
+        }
+    });
+};
