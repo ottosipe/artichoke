82d8508fad0bccbe9d8dcce9258a16ed1cb90efe
Matt Kneiser <mattman@umich.edu>
2013-01-19 09:15:53 -0500

    Updated the splash page

--- public/js/splash.js
+++ public/js/splash.js
@@ -1,6 +1,12 @@
 $('.btn').click(function(){
 	$.post('/create', function(data){
-	  console.log(data);
-	  window.location = "/edit/"+data;
+		console.log($('#link_text').val());
+		if($('#link_text').val()) {
+			console.log($('#link_text').val());
+			window.location = "/edit/"+$('#link_text').val();
+		} else {
+			console.log(data);
+			window.location = "/edit/"+data;
+		}
 	});
 });
