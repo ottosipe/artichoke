558c5fb9354b3c59758b566b58add1bc322329e3
Otto Sipe <ottosipe@gmail.com>
2013-01-18 21:00:48 -0500

    added secret.js

---
+++ server/secret.js
@@ -0,0 +1,17 @@
+module.exports = Object.freeze({
+	db: {
+		user:"user",
+		pass:"pass",
+		url: "linus.mongohq.com",
+		port: 10082,
+		name: "ottosipe"
+	}, 
+	email: {
+        user: "ottosipe@gmail.com",
+        pass: "cedb08cc-a0ed-42cd-9cbe-fbb9b0ab2a8f"
+    },
+    admin: {
+    	user: "user",
+    	pass: "pass"
+    }
+});
