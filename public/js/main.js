24eba41b9d8d2b52e21ee768a7f5cb83c8c2926e
Otto Sipe <ottosipe@gmail.com>
2013-01-18 22:58:51 -0500

    added toxbox and basic style

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,50 @@ $(document).ready(function(){
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/javascript");
 
-    var editor2 = ace.edit("editor2");
-    editor.setTheme("ace/theme/monokai");
-    editor.getSession().setMode("ace/mode/javascript");
+    editor.getSession().selection.on('changeCursor', function(e) {
+		console.log(editor.selection.getCursor())
+		// send socekt update here ***
+	});
+
+	editor.getSession().selection.on('changeSelection', function(e) {
+		console.log(editor.session.getTextRange(editor.getSelectionRange()))
+		// send socekt update here ***
+	});
+
+  editor.commands.addCommand({
+    name: 'save',
+    bindKey: {win: 'Ctrl-S',  mac: 'Command-S'},
+    exec: function(editor) {
+        console.log('Send update to server to save here ***')
+    },
+    readOnly: false // not for readOnly mode
+  });
+
+
+  // tokbox
+  // Initialize API key, session, and token...
+  // Think of a session as a room, and a token as the key to get in to the room
+  // Sessions and tokens are generated on your server and passed down to the client
+  var apiKey = "22586972";
+  var sessionId = "1_MX4yMjU4Njk3Mn4xMjcuMC4wLjF-RnJpIEphbiAxOCAxOToxOToxMSBQU1QgMjAxM34wLjA3MjA1MTA1fg";
+  var token = "T1==cGFydG5lcl9pZD0yMjU4Njk3MiZzaWc9MmE4MTZmMDk4MmZiODI2ODY0NTg0OGRjYjNmNjU2MDY4NWZhZmM0NDpzZXNzaW9uX2lkPXVuZGVmaW5lZCZjcmVhdGVfdGltZT0xMzU4NTY1NTU3JmV4cGlyZV90aW1lPTEzNTg2NTE5NTcmcm9sZT1wdWJsaXNoZXImbm9uY2U9NDAwMDQ4JnNka192ZXJzaW9uPXRiLWRhc2hib2FyZC1qYXZhc2NyaXB0LXYx";
+
+  // Initialize session, set up event listeners, and connect
+  var session = TB.initSession(sessionId);
+  session.addEventListener('sessionConnected', sessionConnectedHandler);
+  //session.connect(apiKey, token);
+
+  function sessionConnectedHandler(event) {
+    var publisher = TB.initPublisher(apiKey, 'tokbox');
+    session.publish(publisher);
+  }
+
+  function exceptionHandler(event) {
+    // Retry session connect
+    if (event.code === 1006 || event.code === 1008 || event.code === 1014) {
+    alert('There was an error connecting. Trying again.');
+    session.connect(apiKey, token);
+    }
+  }
 });
 
